4f53ca2f6eeac06bd7983cf3f0f6aad743462dfc
zeyad <zeyad@zeyads-mbp>
2021-04-24 21:13:14 +0400

    Updated Test.java

--- src/myPackage/Test.java
+++ src/myPackage/Test.java
@@ -2,7 +2,10 @@ package myPackage;
 
 public class Test {
 	public static void main(String[] args) {
-		System.out.println("Hello");
+		System.out.println("I'm getting more of a seared bite on the Impossible Whopper, than I'm getting on the actual Who- no no no no no no no no.... No please don't say that no no no... No. Nonononono. No no nono no no. NONO NO NONO NO NOOOOO. Ladies and gentlemen, I'm not gonna trip. I wanted to come in a 100% hater.\n"
+				+ "I'mma tell you off the top. I wanted to take a bite of this and be like 'ehhh nah nah it it'- I'mma just flat out say it; your Impossible Whopper BK, IS BETTER THAN YOUR ACTUAL WHOPPER BK. YOUR IMPOSSIBLE WHOPPER BK IS BETTER THAN YOUR ACTUAL WHOPPER BK. IT HAS A MORE SEARED TASTE.\n"
+				+ "IT'S MORE OF A COOKOUT FLEX. IT JUST TASTES BETTER. Get over here. You get back in the bag. BK. That bite is so flavorful my m- and the fact that it works so well, it literally compliments the lettuce. Like I'm not even mad that I'm bittin' up, and I'm not even mad, that I'm eating lettuce right now and tomatoes.\n"
+				+ "You can really taste the onion because the sear, on this plant based situation right here, it's so strong and you barely get onion but the onion's there so you get a crunch off it, with soft bun action and SBA? BK, YOU'RE ONTO SOMETHING, AND I LIKE WHERE YOU, YOU GOIN' BK, THE IMPOSSIBLE WHOPPER SON!");
 	}
 	
 }
